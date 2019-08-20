6e044a616882cbf167d1b14fcbf604c0c110f532
Peehii <a180616@gmail.com>
2019-08-20 16:36:18 +0800

    第 1 章，作業 2 - Currency專案，計算美金並顯示

--- app/src/main/java/com/home/currency/MainActivity.java
+++ app/src/main/java/com/home/currency/MainActivity.java
@@ -1,15 +1,45 @@
 package com.home.currency;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.fly.currency.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText ntd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+
+    private void findViews() {
+        ntd = findViewById(R.id.ntd);
+    }
+
+    public void go(View view) {
+        String n = ntd.getText().toString();
+        if (n.equals("")){
+            new AlertDialog.Builder(this)
+                    .setTitle("Problem")
+                    .setMessage("Please enter your NTD amount")
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
+        else{
+            float ntd = Float.parseFloat(n);
+            float result = ntd / 30.9f;
+            new AlertDialog.Builder(this)
+                    .setTitle("Result")
+                    .setMessage("USD is " + result)
+                    .setPositiveButton("OK",null)
+                    .show();
+        }
     }
 }
